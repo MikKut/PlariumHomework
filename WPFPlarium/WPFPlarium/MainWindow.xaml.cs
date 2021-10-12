0874728d700d87e3982eb2fa03fb1ee96f8db0bc
MikKut <69770483+MikKut@users.noreply.github.com>
2021-10-12 17:18:48 +0300

    Add files via upload

---
+++ WPFPlarium/WPFPlarium/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFPlarium
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        
+        private void TurnOnTask1()
+        {
+            InputTextForTasks.Visibility = Visibility.Visible;
+            RectangleTask1.Visibility = Visibility.Visible;
+            FieldTask1.Visibility = Visibility.Visible;
+            CalculateTask1Button.Visibility = Visibility.Visible;
+            Conditions.Visibility = Visibility.Visible;
+            TurnOffStartButtonOfTask1();
+        }
+        private void TurnOffStartButtonOfTask1()
+        {
+            Task_1_1.Visibility = Visibility.Collapsed;
+            ToolBar.Visibility = Visibility.Collapsed;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateTask1(object sender, RoutedEventArgs e)
+        {
+            int? result;
+            result = SyntaxHomework.CountSumOfNumbersDividedByThreeWithoutReminder(FieldTask1.Text);
+            myPopupText.Text = $"The sum of all the numbers divided by three without reminder is {result}";
+        }
+
+
+        private void DoTask1(object sender, RoutedEventArgs e)
+        {
+            TurnOnTask1();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void CalculateTask1Button_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+    
+}
---
+++ WPFPlarium/WPFPlarium/Task1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFPlarium
+{
+    public static partial class SyntaxHomework
+    {
+        #region Task 1.1
+        //Write a program that enters seven values and calculates the sum of integers that are divisible by 3 without a remainder.
+        public static int CountSumOfNumbersDividedByThreeWithoutReminder(string strSequence)
+        {
+            int sum = 0, quatityOfNumbers;
+            bool isParsedSuccessfuly;
+            int[] array;
+            isParsedSuccessfuly = ParseTheArray(strSequence, out array, out quatityOfNumbers);
+            if (isParsedSuccessfuly)
+            {
+                for (int i = 0; i < quatityOfNumbers; i++)
+                {
+                    if (array[i] % 3 == 0)
+                    {
+                        sum += array[i];
+                    }
+
+                }
+                return sum;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        private static bool ParseTheArray(string strSequence, out int[] array, out int quantityOfNumbers)
+        {
+            string[] strArray;
+            strArray = strSequence.Split(' ');
+            quantityOfNumbers = strArray.Length;
+            array = new int[quantityOfNumbers];
+            if (quantityOfNumbers == 0)
+            {
+                return false;
+            }
+            for (int i = 0; i < quantityOfNumbers; i++)
+            {
+                if (!Int32.TryParse(strArray[i], out array[i]))
+                {
+                    return false;
+                }
+            
+            }
+            return true;
+        }
+        #endregion
+    }
+}
