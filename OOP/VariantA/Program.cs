7f474c065ec0556723fea9b011446ae6b5bae5e8
MikKut <69770483+MikKut@users.noreply.github.com>
2021-10-19 17:56:56 +0300

    Add files via upload

---
+++ OOP/VariantA/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace VariantA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Стрелять бегать и кушать и";
+            StrBuilderTasks.MarkTheWordInTheText("йцукен йцукеныд йцукен гоа пыпло йцукен кшойцукенг йцукен", "йцукен");
+            StrBuilderTasks.RemoveVerbsFromString(ref str);
+            Console.WriteLine();
+            Console.WriteLine(str);
+            StrBuilderTasks.FindWordWithSameBase("уйцукен йцукеныд гоа пыпло йцук кшойцукенг");
+        }
+    }
+}
---
+++ OOP/VariantB/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VariantB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pointHendler = new VariantB.PointHandler(4);
+            var point = pointHendler.FindTheNearestPoint(new Point((0, 0)), new VariantB.Point[] { new VariantB.Point(2, 4), new VariantB.Point(1, 1), });
+            Console.WriteLine($"{point.X} + {point.Y}");
+            point = pointHendler.FindTheFurthestPoint(new Point((0, 0)));
+            Console.WriteLine($"{point.X} + {point.Y}");
+            pointHendler.DisplayPointsLyingOnTheLineWithThePoints(point, new Point(1, 2));
+        }
+    }
+}
+ 
