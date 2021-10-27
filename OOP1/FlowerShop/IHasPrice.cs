fd04f81a0aaaea07c7a043265c53dc55b79d7564
MikKut <69770483+MikKut@users.noreply.github.com>
2021-10-27 12:43:04 +0300

    Delete OOP1 directory

--- OOP1/FlowerShop/IHasPrice.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FlowerShop
-{
-    interface IHasPrice
-    {
-        public double TotalPrice { get; }
-        public void MakeMarkup(double quantityOfPercents);
-        public void MakeDiscount(double quantityOfPercents);
-    }
-}
